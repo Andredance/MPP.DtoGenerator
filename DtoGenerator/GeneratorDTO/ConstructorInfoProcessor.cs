e1b8efc9e8aad2eb50fc05b9cf65265c7eaeebf1
andredance <andrey99.09@gmail.com>
2018-10-02 15:57:35 +0300

    implemented method that find constructor with max count of basic typed parameters

---
+++ DtoGenerator/GeneratorDTO/ConstructorInfoProcessor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneratorDTO
+{
+    internal static class ConstructorInfoProcessor
+    {
+        internal static ConstructorInfo GetMaxParameterizedByBasicTypesConstructor(List<ConstructorInfo> allConstructors)
+        {
+            if (allConstructors.Count == 0)
+            {
+                throw new InvalidOperationException("Empty list");
+            }
+    
+            allConstructors = allConstructors.OrderBy(item => item.GetParameters().Length).ToList();
+            foreach (ConstructorInfo constructor in allConstructors)
+            {
+                if (constructor.GetParameters().Any(parameter => ParametersInfoProcessor.isParameterSimple(parameter)))
+                {
+                    return constructor;
+                }
+            }
+
+            throw new InvalidOperationException("Can't find constructor that satisfying conditions");
+        } 
+    }
+}
