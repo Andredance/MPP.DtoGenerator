fafe7635a57cae843caf7f26eb675d8559df7a7e
andredance <andrey99.09@gmail.com>
2018-10-02 15:58:25 +0300

    imlement method that check is parameter simple(basic type)

---
+++ DtoGenerator/GeneratorDTO/ParametersInfoProcessor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneratorDTO
+{
+    internal static class ParametersInfoProcessor
+    {
+        internal static bool isParameterSimple(ParameterInfo parameter)
+        {
+            Type type = parameter.GetType();
+            return type.IsPrimitive || type.Equals(typeof(string));
+        }
+    }
+}
