153d279a0fa052016bb98b0bd6683a0263c6680f
andredance <andrey99.09@gmail.com>
2018-10-03 14:20:10 +0300

    added generator for INT type

---
+++ DtoGenerator/GeneratorDTO/Generators/IntGenerator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneratorDTO.Generators
+{
+    public class IntGenerator : IGenerator
+    {
+        Random random;
+
+        public IntGenerator()
+        {
+            random = new Random();
+        }
+
+        public object Generate()
+        {
+            return random.Next(int.MinValue, int.MaxValue);
+        }
+    }
+}
