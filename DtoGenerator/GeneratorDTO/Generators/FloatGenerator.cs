96c8c1ea4c435c81936e1fe50246a056495a38f4
andredance <andrey99.09@gmail.com>
2018-10-03 14:19:36 +0300

    added generator for float type

---
+++ DtoGenerator/GeneratorDTO/Generators/FloatGenerator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneratorDTO.Generators
+{
+    public class FloatGenerator : IGenerator
+    {
+        public object Generate()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
