58ea86b49f1f61ba6fd30e4b857e5b28ea50d2c6
andredance <andrey99.09@gmail.com>
2018-10-04 16:31:37 +0300

    added Boolean values generator

---
+++ DtoGenerator/GeneratorDTO/Generators/BoolGenerator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneratorDTO.Generators
+{
+    public class BoolGenerator : IGenerator
+    {
+        Random random;
+
+        public BoolGenerator()
+        {
+            random = new Random();
+        }
+
+        public object Generate()
+        {
+            return random.Next(1) == 1 ? true : false;
+        }
+    }
+}
