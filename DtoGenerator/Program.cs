a368424b5a106ab5308b3cc82c7966888604fdc2
andredance <andrey99.09@gmail.com>
2018-10-05 00:15:41 +0300

    simple usage example

--- DtoGenerator/Program.cs
+++ DtoGenerator/Program.cs
@@ -3,13 +3,52 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using GeneratorDTO.Generators;
+using GeneratorDTO;
+using System.Reflection;
 
 namespace DtoGenerator
 {
+    public class A
+    {
+        public B b;
+        public C c;
+    }
+
+    public class B
+    {
+        public C c;
+    }
+
+    public class C
+    {
+        public int a;
+    }
+    public class MoreSimple
+    {
+        public char a;
+        public string b;
+    }
+    public class Simple
+    {
+        public int a;
+        public uint b;
+        public string c;
+        public byte d;
+        public char e;
+        public double f;
+        public float g;
+        public bool h;
+        public List<MoreSimple> list;
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            C c = new C();
+
+            Console.WriteLine(c.a);
+            Console.ReadKey();
         }
     }
 }
